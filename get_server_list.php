a466631e9a02368cfa8da9a3de84d8cd57d7b6b7
caiweihan <caiweihan@local.host>
2017-07-08 18:44:37 +0800

    1

--- get_server_list.php
+++ get_server_list.php
@@ -1,7 +1,7 @@
 ﻿<?php 
 	$returnData->list = array(
 	"1"=>array("name"=>"1区","time"=>"2017-04-06 10:30:00","host"=>"http://172.17.196.195:90/gameindex.php")
-	// "2"=>array("name"=>"2区","time"=>"2017-03-06 17:33:00","host"=>"http://172.17.196.195:90/gameindex2.php")
+	//"2"=>array("name"=>"2区","time"=>"2017-03-06 17:33:00","host"=>"http://172.17.196.195:90/gameindex2.php")
 	);
 	
 	// $returnData->list = array(
